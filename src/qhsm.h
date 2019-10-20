148fcec19976e9fbb0168b95cba7974eb64d04c6
Will Reynolds <will.rnlds@gmail.com>
2019-10-20 00:55:42 -0500

    working on requirements

--- src/qhsm.h
+++ src/qhsm.h
@@ -1,5 +1,44 @@
 #ifndef _QHSM_H
 #define _QHSM_H
 
+typedef struct Qhsm Qhsm;
+typedef struct QhsmTransit QhsmTransit;
+
+typedef enum QhsmResult {
+    QHSM_RESULT_IGNORED,
+    QHSM_RESULT_CONSUMED,
+    QHSM_RESULT_DEFERRED,
+} QhsmResult;
+
+typedef enum QhsmEvent {
+    QHSM_EVENT_ENTRY,
+    QHSM_EVENT_INIT,
+    QHSM_EVENT_EXIT,
+    QHSM_EVENT_FIRST_AVAILABLE,
+} QhsmEvent;
+
+typedef enum QhsmTransitType {
+    QHSM_TRANSIT_TYPE_NORMAL,
+} QhsmTransitType;
+
+typedef enum QhsmTransitResult {
+    QHSM_TRANSIT_RESULT_NONE,
+} QhsmTransitResult;
+
+typedef QhsmTransitResult (*QhsmTransitHandler)(Qhsm *source, QhsmTransit *transit);
+
+struct QhsmTransit {
+    Qhsm *target;
+    Qhsm *exit;
+    QhsmTransitType type;
+    QhsmTransitHandler handler;
+};
+
+typedef QhsmResult (*QhsmEventHandler)(Qhsm *self, QhsmEvent *event, QhsmTransit *transit);
+
+struct Qhsm {
+    Qhsm *parent;
+    QhsmEventHandler handler;
+};
 
 #endif // _QHSM_H
--- test/test_qhsm.c
+++ test/test_qhsm.c
@@ -2,24 +2,36 @@
 #include "qhsm.h"
 
 //  Qhsm
-//      - a Qhsm can have a Parent Qhsm. (*Qhsm)parent
-//      - a Qhsm can have an event handler. (*EventHandler(*Event))event_handler
+//      - IMPLICIT: a Qhsm can have a Parent Qhsm. (*Qhsm)parent
+//      - IMPLICIT: a Qhsm can have an event handler. (*EventHandler(*Event))event_handler
+
+//  QHsmDoResult QHsmDo(*QHsm,*QHsmEvent)
 //      - if library is passed an internal event it will throw an error
 //      - if library is passed a null event it will throw an error
 //      - if library is passed a null QHsm it will throw an error
-//  QHsmDoResult QHsmDo(*QHsm,*QHsmEvent)
-//      - main function
+
+//  boolean QhsmStateIsActive(*Qhsm);
+//      - if state is an active leaf or parent of an active leaf returns true
+//      - else returns false
+
 //  QhsmEventHandler(EventResult(Event))
-//      - takes events and returns results and transitions (transition through a parameter)
-//      - for entry and exit, result is ignored and treated as ignored.
-//      - for init events, result is ignored and treated as consumed.
+//      - IMPLICIT: takes events and returns results and transitions (transition through a parameter)
+//      - on first event, root/top state becomes active leaf
+//      - on event becoming active leaf, event handler gets hit with entry and then init
+//      - for entry and exit, result is discarded and treated as ignored.
+//          - entry transit ignored even if handler returns consumed
+//          - exit transit ignored even if handler returns consumed
+//      - for init events, result is discarded and treated as consumed.
+//          - init transit is taken even if handler returns ignored
 //      - for all other events:
-//          - if result is ignored, it will be passed to parent (if exists)
-//          - if result is consumed, transition will be taken (if given)
+//          - if result is ignored, it will be passed to parent (if exists, and doesn't contain unprocessed orthogonal regions) or leaf-most unprocessed active state in another region (if exists)
+//          - if all states ignore event, QhsmDo returns ignored
+//          - if result is consumed, transition will be taken (if given) and 
 //          - if result is deferred:
 //              - event will be passed to remaining orthogonal regions
 //              - if event is not consumed it will be saved
 //              - saved events are rethrown in FIFO order after next transition
+
 //  QhsmEventResult
 //      - OHSM_EVENT_RESULT_IGNORED
 //          - will pass event to parent if parent exists
@@ -27,6 +39,7 @@
 //          - will stop passing event
 //      - OHSM_EVENT_RESULT_DEFERRED
 //          - will defer event if it is not consumed in an orthogonal region
+
 //  QHsmTransition
 //      - can have a destination QHsm
 //      - can have a subtarget
@@ -36,8 +49,10 @@
 //          - TERMINATE will transition to target and then exit entire QHsm tree
 //      - can have an exit QHsm
 //          - if given and in active tree transition will exit down the tree until it has exitted the exit QHsm before transitioning to target
+
 //  QhsmEvent
 //   - has an EventId (ENTRY/INIT/EXIT/application)
+
 //  QhsmEventId
 //      - QHSM_EVENT_ID_ENTRY
 //          - passed when a state first becomes active
